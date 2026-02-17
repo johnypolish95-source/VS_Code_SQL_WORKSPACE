17f65ae37723a7ce73be22675130570143bc2a7c
Jan <johnypolish95@gmail.com>
2026-02-17 17:14:06 +0100

    Query 1

--- Project_SQL_2026/Query1_top_paying_jobs.sql
+++ Project_SQL_2026/Query1_top_paying_jobs.sql
@@ -1,10 +1,23 @@
 USE DataJobs_Database
 GO
 
-SELECT TOP 5000 *
-FROM job_postings_fact
+SELECT TOP 1000
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    company_dim.name AS company_name,
 
-WHERE job_title LIKE '%Data%' AND job_title LIKE '%Analyst%' AND salary_year_avg IS NOT NULL
-AND job_title_short NOT LIKE '%Senior%' AND salary_year_avg >= 50000
+    RANK() OVER (PARTITION BY job_location ORDER BY salary_year_avg DESC) AS job_rank
+
+FROM
+    job_postings_fact
+
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE job_title_short = 'Data Analyst' AND (job_location IN ('Poland', 'Anywhere')) AND salary_year_avg IS NOT NULL
+
 
 ORDER BY salary_year_avg DESC;
