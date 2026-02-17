aec1ff90d83c339e5f21b43520b3b3dbd977a68a
Jan <johnypolish95@gmail.com>
2026-02-17 22:38:11 +0100

    Query5_Most_Optimal_Skills_v1.0

--- Project_SQL_2026/Query5_most_optimal_skills.sql
+++ Project_SQL_2026/Query5_most_optimal_skills.sql
@@ -1,14 +1,43 @@
 USE DataJobs_Database -- Replace with your actual database name
 GO
 
-SELECT TOP 25
-    skills_dim.skills,
-    ROUND(AVG(salary_year_avg), 0) AS avg_salary
-FROM job_postings_fact
 
-INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL -- AND (job_location IN ('Poland'))
-AND job_work_from_home = 1 -- Filter for remote jobs
-GROUP BY skills_dim.skills
-ORDER BY avg_salary DESC;
+WITH skills_demand AS (
+    SELECT
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Data Analyst' AND job_work_from_home = 1
+    AND salary_year_avg IS NOT NULL --AND (job_location IN ('Poland'))
+    GROUP BY skills_dim.skill_id, skills_dim.skills
+),
+
+
+ average_salary AS (
+    SELECT
+        skills_job_dim.skill_id,
+        ROUND(AVG(salary_year_avg), 0) AS avg_salary
+    FROM job_postings_fact
+
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE job_title_short = 'Data Analyst' AND salary_year_avg IS NOT NULL -- AND (job_location IN ('Poland'))
+    AND job_work_from_home = 1 -- Filter for remote jobs
+    GROUP BY skills_job_dim.skill_id
+)
+
+SELECT TOP 25
+        skills_demand.skill_id,
+         skills_demand.skills,
+         skills_demand.demand_count,
+         average_salary.avg_salary
+FROM skills_demand
+INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE demand_count > 30 AND avg_salary > 50000
+ORDER BY demand_count DESC, avg_salary DESC;
+
+
